63e245276172231caff10f0ba342f9563afd5249
FranciscoDarlan <darlann_c_jpa@hotmail.com>
2024-04-08 15:14:20 -0300

    colocando comentarios da funcionalidade do codigo.

--- js/app.js
+++ js/app.js
@@ -50,18 +50,25 @@ function alterarStatus3() {
 
 
 function alterarStatus(id) {
-    let gameClicado = document.getElementById(`game-${id}`);
+
+    // todas as variaveis que vou manipular >>   
+    let gameClicado = document.getElementById(`game-${id}`); // preciso concatenar com valor do paramentro da funcção pego no html dentro do parentese 
     let imagem = gameClicado.querySelector('.dashboard__item__img');
     let botao = gameClicado.querySelector('.dashboard__item__button');
-    let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
+    // não vou precisaer da variavel do nome do jogo>>     let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
 
-    alert(nomeJogo.textContent);
+    // não preciso desse alerta >>    alert(nomeJogo.textContent); // alerta com nome somente do valor dentro da tag poderia ser .innerHTML também.
 
+
+    // verificando com uma consição booleana verdadeira ou false (true) end (false) if e else.>>   
     if (imagem.classList.contains('dashboard__item__img--rented')) {
+        //se a consição do if for verdadeira vai entrar aqui e fazer o que tem aqui dentro >>
         imagem.classList.remove('dashboard__item__img--rented');
         botao.classList.remove('dashboard__item__button--return');
         botao.textContent = 'Alugar';
     } else {
+        //se a consição do if for falsa vai entrar aqui e fazer o que tem aqui dentro >>
+
         imagem.classList.add('dashboard__item__img--rented');
         botao.classList.add('dashboard__item__button--return');
         botao.textContent = 'Devolver';
